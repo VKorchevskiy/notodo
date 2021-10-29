0b8c2d43d178810cb4a0dc2828869d18675d3fe3
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-10-29 13:40:41 +0300

    add id

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,33 +2,35 @@ import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  const [todo, setTodo] = useState();
+  const [todoName, setTodoName] = useState("");
+  const [id, setId] = useState(0);
   const [todos, setTodos] = useState([]);
 
-  const handleChangeTodo = (e) => setTodo(e.target.value);
+  const handleChangeTodo = (e) => setTodoName(e.target.value);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setTodos([todo, ...todos]);
+    setId(id + 1)
+    setTodos([{id: id, name: todoName}, ...todos]);
   }
 
   function handleDelete(todo) {
-    setTodos((state) => state.filter((t) => t !== todo));
+    setTodos((state) => state.filter((t) => t.id !== todo.id));
   }
 
   return (
     <div className="App">
       <h1>Todo list</h1>
       <form className="form" onSubmit={handleSubmit}>
-        <input type="text" value={todo || ""} onChange={handleChangeTodo} />
+        <input type="text" value={todoName || ""} onChange={handleChangeTodo} />
         <input type="submit" value="Добавить" />
       </form>
       <ul className="list">
-        {todos.map((todo, i) => (
-          <li key={`${todo}${i}`} className="item">
+        {todos.map((todo) => (
+          <li key={`${todo.name}${todo.id}`} className="item">
             <div className="todo">
-              <p>{todo}</p>
-              <input type="button" value="Удалить" onClick={() => handleDelete(`${todo}`)} />
+              <p>{todo.name}</p>
+              <input type="button" value="Удалить" onClick={() => handleDelete(todo)} />
             </div>
           </li>
         ))}
