ffee4cdf8cf7e602e8c649486b6eca8332ffba8c
Vladimir Korchevskiy <burg2007@yandex.ru>
2021-10-31 18:34:07 +0300

    todo

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,41 @@
-import './App.css';
-import React, { useState } from 'react';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
   const [todoName, setTodoName] = useState("");
   const [id, setId] = useState(0);
+  const [currentTodo, setCurrentTodo] = useState(null);
   const [todos, setTodos] = useState([]);
 
   const handleChangeTodo = (e) => setTodoName(e.target.value);
 
   function handleSubmit(e) {
     e.preventDefault();
-    setId(id + 1)
-    setTodos([{id: id, name: todoName}, ...todos]);
+    setId(id + 1);
+    if (currentTodo === null) {
+      setTodos([{ id: id, name: todoName }, ...todos]);
+    } else {
+      setTodos((state) =>
+        state.map((t) => {
+          if (t.id !== currentTodo.id) {
+            return t;
+          } else {
+            return { id: t.id, name: todoName };
+          }
+        })
+      );
+      setCurrentTodo(null);
+    }
+    setTodoName("");
   }
 
   function handleDelete(todo) {
     setTodos((state) => state.filter((t) => t.id !== todo.id));
+  }
+
+  function handleEdit(todo) {
+    setCurrentTodo(todo);
+    setTodoName(todo?.name)
   }
 
   return (
@@ -23,14 +43,26 @@ function App() {
       <h1>Todo list</h1>
       <form className="form" onSubmit={handleSubmit}>
         <input type="text" value={todoName || ""} onChange={handleChangeTodo} />
-        <input type="submit" value="Добавить" />
+        <input
+          type="submit"
+          value={currentTodo === null ? `Добавить` : `Редактировать`}
+        />
       </form>
       <ul className="list">
         {todos.map((todo) => (
           <li key={`${todo.name}${todo.id}`} className="item">
             <div className="todo">
-              <p>{todo.name}</p>
-              <input type="button" value="Удалить" onClick={() => handleDelete(todo)} />
+              <p style={{margin: 0, padding: 0}}>{todo.name}</p>
+              <input
+                type="button"
+                value="Удалить"
+                onClick={() => handleDelete(todo)}
+              />
+              <input
+                type="button"
+                value="Редактировать"
+                onClick={() => handleEdit(todo)}
+              />
             </div>
           </li>
         ))}
